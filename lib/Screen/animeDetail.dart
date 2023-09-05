9c89d24fb29f1ff109835108fe22b2d9cdab4bd8
panachat <Panachat1183@gmail.com>
2023-09-06 01:16:12 +0700

    detail

--- lib/Screen/animeDetail.dart
+++ lib/Screen/animeDetail.dart
@@ -19,83 +19,84 @@ class Details extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF0B1622),
-      appBar: AppBar(
-        title: Text(title ?? ''),
-        backgroundColor: Color(0xFF2b2d42),
-      ),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            height: 250, // ปรับความสูงของรูปภาพหลัก
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                  imageUrl ?? '',
-                ),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 7.0, sigmaY: 7.0),
-              child: Container(
-                decoration: BoxDecoration(color: Colors.white.withOpacity(0.0)),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 40, // ปรับตำแหน่งรูปภาพที่อยู่กลาง
-            left: 0,
-            right: 0,
-            child: Center(
-              child: Container(
-                width: 195,
-                height: 260, // ปรับความสูงของรูปภาพที่อยู่กลาง
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: Image.network(
-                    imageUrl ?? '',
-                    fit: BoxFit.cover,
+        backgroundColor: Color(0xFF0B1622),
+        appBar: AppBar(
+          title: Text(title ?? ''),
+          backgroundColor: Color(0xFF2b2d42),
+        ),
+        body: ListView(
+          children: [
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: 250, // ปรับความสูงของรูปภาพหลัก
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        imageUrl ?? '',
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 7.0, sigmaY: 7.0),
+                    child: Container(
+                      decoration:
+                          BoxDecoration(color: Colors.white.withOpacity(0.0)),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ),
-          Center(
-              child: Card(
-            color: Color(0xFF151f2e),
-            margin: EdgeInsets.all(20),
-            child: SingleChildScrollView(
-              // ปรับความสูงของ ListView
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      title ?? '',
-                      style: TextStyle(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
+                Positioned(
+                  top: 40, // ปรับตำแหน่งรูปภาพที่อยู่กลาง
+                  left: 0,
+                  right: 0,
+                  child: Center(
+                    child: Container(
+                      width: 195,
+                      height: 260, // ปรับความสูงของรูปภาพที่อยู่กลาง
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.network(
+                          imageUrl ?? '',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
                   ),
-                  ListTile(
-                    title: Text(
-                      "Type: ${type}",
-                      style: TextStyle(color: Colors.white),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 450, 0, 0),
+                      padding: EdgeInsets.fromLTRB(0, 0, 290, 0),
+                      child: Text(
+                        "Description",
+                        style: TextStyle(color: Colors.amber),
+                      ),
                     ),
-                    subtitle: Text(
-                      subtitle ?? '',
-                      style: TextStyle(fontSize: 18.0),
-                    ),
-                  ),
-                  // เพิ่ม Widgets อื่น ๆ ตามต้องการ
-                ],
-              ),
+                    Container(
+                      margin: EdgeInsets.all(20),
+                      child: Card(
+                        color: Color(0xFF151f2e),
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: Text(
+                                description ?? "",
+                                style: TextStyle(color: Color(0XFF8a919c)),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
             ),
-          ))
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
